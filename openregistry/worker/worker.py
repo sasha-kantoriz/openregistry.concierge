ea30ece082a3b5c60c7cb4ae1ba66f139de2a747
Scandie <grizly.vl@gmail.com>
2017-08-11 15:00:39 +0300

    add basic error handling

--- openregistry/worker/worker.py
+++ openregistry/worker/worker.py
@@ -5,9 +5,10 @@ import time
 import yaml
 
 from couchdb import Database, Session
+from couchdb.http import RETRYABLE_ERRORS
 
 from openprocurement_client.registry_client import RegistryClient
-from openprocurement_client.exceptions import InvalidResponse, Forbidden
+from openprocurement_client.exceptions import InvalidResponse, Forbidden, RequestFailed
 
 PWD = os.path.dirname(os.path.realpath(__file__))
 logger = logging.getLogger(__name__)
@@ -38,66 +39,79 @@ class BotWorker(object):
 
     def get_lots(self, view):
         logger.info("Getting lots")
-        return ({"data": {
-            'id': lot.id,
-            'assets': lot.value['assets'],
-            'status': lot.value['status']}}for lot in self.db.view(view)
-                if lot.value['status'] in ['waiting', 'dissolved'])
+        try:
+            return ({"data": {
+                'id': lot.id,
+                'assets': lot.value['assets'],
+                'status': lot.value['status']}}for lot in self.db.view(view)
+                    if lot.value['status'] in ['waiting', 'dissolved'])
+        except Exception, e:
+            ecode = e.args[0]
+            if ecode in RETRYABLE_ERRORS:
+                logger.error("Error while getting lots: {}".format(e))
 
     def check_lot_assets(self, lot):
         assets = lot['data']['assets']
         lot_status = lot['data']['status']
 
-        for asset in assets:
-            asset = self.assets_client.get_asset(asset).data
-            if asset.status != 'pending':
-                # if lot_status == "waiting" or asset.relatedLot == lot['data']['id']: TODO: wait for proper asset serialization
-                if lot_status == "waiting" or lot_status == "dissolved":
-                    return False
+        for asset_id in assets:
+            try:
+                asset = self.assets_client.get_asset(asset_id).data
+            except RequestFailed:
+                logger.error('Falied to get asset')
+                raise RequestFailed
+            else:
+                logger.info('Successfully get asset {}'.format(asset_id))
+                if asset.status != 'pending':
+                    # if lot_status == "waiting" or asset.relatedLot == lot['data']['id']: # TODO: wait for proper asset serialization
+                    if lot_status == "waiting" or lot_status == "dissolved":
+                        return False
         return True
 
-    def patch_assets(self, lot, status):
-
+    def patch_assets(self, lot, status, related_lot=None):
         for asset_id in lot['data']['assets']:
-            asset = {"data": {"id": asset_id, "status": status, "relatedLot": lot['data']['id']}}
+            asset = {"data": {"id": asset_id, "status": status, "relatedLot": related_lot}}
             try:
                 self.assets_client.patch_asset(asset)
-            except (InvalidResponse, Forbidden):
+            except (InvalidResponse, Forbidden, RequestFailed):
                 logger.error("Failed to patch asset {} to {}".format(asset_id, status))
+                return False
             else:
                 logger.info("Successfully patched asset {} to {}".format(asset_id, status))
+        return True
 
     def patch_lot(self, lot, status):
 
         lot['data']['status'] = status
         try:
             self.lots_client.patch_lot(lot)
-        except (InvalidResponse, Forbidden):
+        except (InvalidResponse, Forbidden, RequestFailed):
             logger.error("Failed to patch lot {} to {}".format(lot['data']['id'], status))
+            return False
         else:
             logger.info("Successfully patched lot {} to {}".format(lot['data']['id'], status))
+            return True
 
     def process_lots(self, lots):
+        if not lots:
+            logger.info("Could not get any lots")
+            return
         logger.info("Get lots")
         for lot in lots:
             logger.info("Processing lot {}".format(lot['data']['id']))
-            assets_available = self.check_lot_assets(lot)
-            self.make_patch(lot, assets_available)
+            try:
+                assets_available = self.check_lot_assets(lot)
+            except RequestFailed:
+                logger.info("Due to fail in getting assets, lot {} is skipped".format(lot['data']['id']))
+            else:
+                self.make_patch(lot, assets_available)
         logger.info("Processed all lots")
 
     def make_patch(self, lot, assets_available):
-        # if assets_available and lot['data']['status'] == 'waiting':
-        #     self.patch_lot(lot, "active.pending")
-        #     self.patch_assets(lot, 'active')
-        # else:
-        #     if lot['data']['status'] == 'waiting':
-        #         self.patch_lot(lot, "invalid")
-        #     elif lot['data']['status'] == 'dissolved':
-        #         self.patch_assets(lot, 'pending')
         if lot['data']['status'] == 'waiting':
             if assets_available:
                 self.patch_lot(lot, "active.pending")
-                self.patch_assets(lot, 'active')
+                self.patch_assets(lot, 'active', lot['data']['id'])
             else:
                 self.patch_lot(lot, "invalid")
         elif lot['data']['status'] == 'dissolved' and not assets_available:
@@ -112,7 +126,7 @@ class BotWorker(object):
 
 def main():
 
-    config_path = os.path.join(PWD, "../bot_conf.yaml")
+    config_path = os.path.join(PWD, "bot_conf.yaml")
     config = yaml.load(open(config_path))
     BotWorker(config).run()
 
